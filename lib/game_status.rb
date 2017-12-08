e92adca1bdf125b6de28e2ff649a36f0f6e6d19a
Joshua Clarence Sadsad <joshsad2@gmail.com>
2017-12-08 23:20:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,8 +4,10 @@ def position_taken?(board, index)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |token|
-    board[token[0]] == "X" && board[token[1]] == "X" && board[token[2]] == "X" && position_taken?(board, token[0] == "X")
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
